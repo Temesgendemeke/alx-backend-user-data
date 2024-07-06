60a2240c6bbd899ba562d40cf76f8f1468da2514
Temesgen dora <tdemeke36@gmail.com>
2024-07-06 03:51:58 -0700

    fixed: task 1 #2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,5 @@ Main file
 """
 import re
 
-
 def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     return re.sub(f"({'|'.join(fields)})=([^{separator}]*)", f"\\1={redaction}", message)
