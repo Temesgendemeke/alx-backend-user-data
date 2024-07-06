4816bfd023c18eea6180b3ff01b527273928ef2d
Temesgen dora <tdemeke36@gmail.com>
2024-07-06 11:24:59 -0700

    fixed: task 1 #3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,31 @@
 Main file
 """
 import re
+import logging
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
-     """ returns the log message obfuscated """
-     for f in fields:
-        message = re.sub(f'{f}=.*?{separator}',
-                         f'{f}={redaction}{separator}', message)
-     return message
+
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
+   """ returns the log message obfuscated """
+   for field in fields:
+      message = re.sub(f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message)
+   return message
+ 
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        """" constructor method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records"""
+        NotImplementedError
+        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
