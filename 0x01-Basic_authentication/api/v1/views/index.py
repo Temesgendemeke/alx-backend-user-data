6ca4e8b687782fecda9c0d262c03de762dffb574
Temesgen dora <tdemeke36@gmail.com>
2024-07-09 13:39:14 -0700

    done: task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized_user() -> str:
+    """ GET /api/v1/unauthorized"""
+    return abort(401)
