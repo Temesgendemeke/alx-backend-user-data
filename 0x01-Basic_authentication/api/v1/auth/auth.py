3607218043e0868b913f9856a06aadf156b23341
Temesgen dora <tdemeke36@gmail.com>
2024-07-10 03:10:28 -0700

    done: task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header"""
-        return request
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current user"""
