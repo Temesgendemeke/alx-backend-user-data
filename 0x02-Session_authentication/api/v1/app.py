fbd0101f42a23a4ae31f41077765876611c9f703
Temesgen dora <tdemeke36@gmail.com>
2024-07-11 13:14:46 -0700

    done: user

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -20,7 +21,8 @@ if os.getenv("AUTH_TYPE") == 'auth':
 if os.getenv("AUTH_TYPE") == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
+if os.getenv("AUTH_TYPE") == 'session_auth':
+    auth = SessionAuth()
 
 @app.before_request
 def pre_request():
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """ SessionAuth class
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ User id for session id """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
