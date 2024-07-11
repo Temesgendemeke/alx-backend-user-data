0eeb0f9e033358dc8f0cd1a1e2908f2d879a2123
Temesgen dora <tdemeke36@gmail.com>
2024-07-11 06:45:33 -0700

    done: task100

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,15 +9,18 @@ class Auth:
     """ Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Require auth"""
-        if path is None or excluded_paths is None or excluded_paths == []:
+        """Require auth with support for wildcard '*' at the end of excluded paths."""
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
-        if path[-1] != '/':
+        if not path.endswith('/'):
             path += '/'
-        if path in excluded_paths:
-            return False
-        else:
-            return True
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path or path == excluded_path + '/':
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header"""
