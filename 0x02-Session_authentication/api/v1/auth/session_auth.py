79443ade0205502f3493d615a84287b16b976665
Temesgen dora <tdemeke36@gmail.com>
2024-07-11 13:56:08 -0700

    done: task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -27,7 +28,9 @@ class SessionAuth(Auth):
         return self.user_id_by_session_id.get(session_id)
     
     def current_user(self, request=None):
-        """ Current user """
+        """ Current user based on session ID """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
-        return user_id
+        if user_id:
+            return User.get(user_id)
+        return None
