6c31240d864842d95abd17589f541fea13c2f735
Temesgen dora <tdemeke36@gmail.com>
2024-07-17 08:26:07 -0700

    fixed: login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -37,10 +37,9 @@ def login() -> str:
     if not auth.valid_login(email, password):
         abort(401)
     session_id = auth.create_session(email)
-    if session_id:
-        response = jsonify({"email": email, "message": "logged in"})
-        response.set_cookie("session_id", session_id)
-        return response
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 @app.route("/sessions", methods=['DELETE'], strict_slashes=False)
