f2e92140a2a9a29478c9d76f9d28d4a9e8657479
Temesgen dora <tdemeke36@gmail.com>
2024-07-17 09:08:15 -0700

    fixed: logout [typos]

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,10 +83,12 @@ class Auth:
         """ reset password token"""
         try:
             user = self._db.find_user_by(email=email)
-            user.reset_token = _generate_uuid()
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
             return user.reset_token
+        
         except NoResultFound:
-            raise ValueError
+            raise ValueError()
 
     def update_password(self, reset_token: str, password: str):
         """ update password"""
