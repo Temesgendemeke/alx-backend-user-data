8b00998c5ed2d91e55907906f4364426a9eda943
Temesgen dora <tdemeke36@gmail.com>
2024-07-17 08:00:11 -0700

    fixed: auth.py #2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,16 +24,19 @@ class Auth:
         """ constructer"""
         self._db = DB()
 
-    def register_user(self, email, password) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """Register a user
         """
         try:
-            if self._db.find_user_by(email=email):
-                raise ValueError(f"User {email} already exists")
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
+            pass
 
-    def valid_login(self, email, password) -> bool:
+        user = self._db.add_user(email, _hash_password(password))
+        return user
+
+    def valid_login(self, email: str, password: str) -> bool:
         """Validate user login
         """
         try:
@@ -59,7 +62,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id) -> Union[User, None]:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """ get user from session id"""
         try:
             user = self._db.find_user_by(session_id=session_id)
